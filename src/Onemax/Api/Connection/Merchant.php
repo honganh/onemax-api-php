fe9829ec392f99324d4ffff76c6d41f191d5ecdf
Hong Anh <pndhainam@hotmail.com>
2015-10-19 12:35:22 +0700

    Update Merchant.php

--- src/Onemax/Api/Connection/Merchant.php
+++ src/Onemax/Api/Connection/Merchant.php
@@ -9,7 +9,7 @@ use Onemax\Api\Connection\Client\Authentication;
 
 class Merchant extends Connector implements OnemaxInterface
 {
-	public function __construct($config ) 
+	public function __construct($config=[] ) 
 	{
 		$client = new Authentication( $config );
 		parent::__construct($client);
