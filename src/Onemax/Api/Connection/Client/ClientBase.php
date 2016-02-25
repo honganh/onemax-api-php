a9650836e2b9c8ffc2f9c1b8afedd5c11184c8e3
Hong Anh <pndhainam@hotmail.com>
2016-02-25 18:00:32 +0700

    Update ClientBase.php

--- src/Onemax/Api/Connection/Client/ClientBase.php
+++ src/Onemax/Api/Connection/Client/ClientBase.php
@@ -1,13 +1,16 @@
 <?php
 namespace Onemax\Api\Connection\Client;
 use Onemax\Api\Connection\Client\ClientInterface;
-abstract class ClientBase implements ClientInterface{
+abstract class ClientBase implements ClientInterface
+{
 	protected $configs = array();
 	protected $client = null;
-	protected function __construct( $configs ) {
+	protected function __construct( $configs ) 
+	{
 		$this->configs = array_merge($this->configs, $configs);
 	}
-	public function getClient() {
+	public function getClient() 
+	{
 		return $this->client;
 	}
 }
